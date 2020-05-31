ee3d69e50a84f360a30de242ccf124b4b46baede
EscritorioGonkz <elgonkz@hotmail.es>
2020-05-31 14:09:57 -0500

    dbhelper.py - Clase DBBitso comentada.

--- dbhelper.py
+++ dbhelper.py
@@ -48,7 +48,7 @@ class DBUsers:
 #         self.conn.execute(Messagedidx)
 #         self.conn.execute(Dateidx)
 #         self.conn.commit()
-# 
+#
 #     def add_message(self, UId, Message, Date):
 #         stmt = "Insert into mensajes (UId, Message, Date) values(?, ?, ?)"
 #         args = (UId, Message, Date)
@@ -75,36 +75,36 @@ class DBUsers:
 #         return last
 
 
-class DBBitso:
-    def __init__(self, dbname="Data/Db/dbbitso.sqlite"):
-        self.dbname = dbname
-        self.conn = sqlite3.connect(dbname)
-
-    def setup(self):
-        tblstmt = "CREATE TABLE IF NOT EXISTS bitso (UId int, Bitso0 text, Bitso1 text)"
-        UIdidx = "CREATE INDEX IF NOT EXISTS UIdIndex ON bitso (UId ASC)"
-        Bitso0idx = "CREATE INDEX IF NOT EXISTS Bitso0Index ON bitso (Bitso0 ASC)"
-        Bitso1idx = "CREATE INDEX IF NOT EXISTS Bitso1Index ON bitso (Bitso1 ASC)"
-        self.conn.execute(tblstmt)
-        self.conn.execute(UIdidx)
-        self.conn.execute(Bitso0idx)
-        self.conn.execute(Bitso1idx)
-        self.conn.commit()
-
-    def add_bitso(self, UId, Bitso0, Bitso1):
-        stmt = "Insert into bitso (UId, Bitso0, Bitso1) values(?, ?, ?)"
-        args = (UId, Bitso0, Bitso1)
-        self.conn.execute(stmt, args)
-        self.conn.commit()
-
-    def get_bitso(self, UId):
-        stmt = "SELECT * FROM bitso WHERE UId = (?)"
-        args = (UId, )
-        self.conn.execute(stmt, args)
-        self.conn.commit()
-
-    def delete_bitso(self, UId):
-        stmt = "DELETE FROM bitso WHERE UId = (?)"
-        args = (UId, )
-        self.conn.execute(stmt, args)
-        self.conn.commit()
+# class DBBitso:
+#     def __init__(self, dbname="Data/Db/dbbitso.sqlite"):
+#         self.dbname = dbname
+#         self.conn = sqlite3.connect(dbname)
+#
+#     def setup(self):
+#         tblstmt = "CREATE TABLE IF NOT EXISTS bitso (UId int, Bitso0 text, Bitso1 text)"
+#         UIdidx = "CREATE INDEX IF NOT EXISTS UIdIndex ON bitso (UId ASC)"
+#         Bitso0idx = "CREATE INDEX IF NOT EXISTS Bitso0Index ON bitso (Bitso0 ASC)"
+#         Bitso1idx = "CREATE INDEX IF NOT EXISTS Bitso1Index ON bitso (Bitso1 ASC)"
+#         self.conn.execute(tblstmt)
+#         self.conn.execute(UIdidx)
+#         self.conn.execute(Bitso0idx)
+#         self.conn.execute(Bitso1idx)
+#         self.conn.commit()
+#
+#     def add_bitso(self, UId, Bitso0, Bitso1):
+#         stmt = "Insert into bitso (UId, Bitso0, Bitso1) values(?, ?, ?)"
+#         args = (UId, Bitso0, Bitso1)
+#         self.conn.execute(stmt, args)
+#         self.conn.commit()
+#
+#     def get_bitso(self, UId):
+#         stmt = "SELECT * FROM bitso WHERE UId = (?)"
+#         args = (UId, )
+#         self.conn.execute(stmt, args)
+#         self.conn.commit()
+#
+#     def delete_bitso(self, UId):
+#         stmt = "DELETE FROM bitso WHERE UId = (?)"
+#         args = (UId, )
+#         self.conn.execute(stmt, args)
+#         self.conn.commit()
