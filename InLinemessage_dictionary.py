78066306efb4c1b8110d2f46058cf42352da8677
gonkz <elgonkz@hotmail.es>
2020-08-24 20:04:11 -0500

    Testing

--- InLinemessage_dictionary.py
+++ InLinemessage_dictionary.py
@@ -1,7 +1,7 @@
 #Lista de textos
 
 mensajes = {
-    'start':'Esta es una aplicación de pruebas por lo que al hacer uso de la misma usted esta de acuerdo con lo siguiente:\n\
+'start':'Esta es una aplicación de pruebas por lo que al hacer uso de la misma usted esta de acuerdo con lo siguiente:\n\
 -Usted es el único responsable por su uso.\n\
 -Usted hace uso de esta aplicación bajo su propio riesgo.\n\
 -No se garantiza el correcto funcionamiento de la misma.\n\
@@ -14,56 +14,85 @@ para mejorar el servicio:\n\
 -Código de lenguaje.\n\
 Para más información por favor visite:\n\
 https://core.telegram.org/bots/api#use',
-    'acepto':'Muy bien, ahora puede acceder a alguna de las siguientes opciones:\n\
-Libros disponibles: Muestra una lista de los libros disponibles.\n\
+'MenuBitso':'Muy bien, ahora puede acceder a alguna de las siguientes opciones:\n\
+Libros disponibles: Muestra una lista de los mercados disponibles.\n\
 Ticker: Muestra la información del libro seleccionado\n\
-Libro de ordenes: Muestra la lista de las ordenes del libro seleccionado.\n\
-Intercambios: Muestra una lista con los intercambios recientes de libro seleccionado.\n ',
-    'noacepto':'Si cambia de opinión presione el siguiente botón.',
-    'availablebooksr':'¿Qué desea hacer ahora?¿Ver otra vez los codigos o regresar?',
-    'ticker':'Por favor eliga alguno de los libros disponibles.',
-    'tickere':'¿Qué desea hacer ahora?\n¿Ver otro libro o regresar a la API Pública?',
-    'orderbookr':'¿Qué parte del libro desea ver?\n¿Las compras o las ventas?',
-    'trades':'Por favor eliga algun de los libros disponibles.',
-    'tradesr':'¿Que desea hacer ahora?\n¿Regresar a Trades o regresar a la API Pública'
+Libros de ordenes: Muestra la lista de las ordenes del libro seleccionado.\n\
+Intercambios: Muestra una lista con los intercambios recientes de libro seleccionado.\n\
+Inicio: Regresar el menú inicial.\n',
+'acepto':'Muy bien, ahora puede seleccionar alguna de las siguientes opciones:\n\
+Menu Bitso: Muestra el menu de acciones para bitso.\n\
+Configuracion: Muestra los ajustes que puede realizar a este bot.\n ',
+'noacepto':'Si cambia de opinión presione el siguiente botón.',
+'availablebooksr':'¿Qué desea hacer ahora?¿Ver otra vez los codigos o regresar?',
+'ticker':'Por favor eliga alguno de los libros disponibles.',
+'tickere':'¿Qué desea hacer ahora?\n¿Ver otro libro o regresar a la API Pública?',
+'orderbookr':'¿Qué parte del libro desea ver?\n¿Las compras o las ventas?',
+'trades':'Por favor eliga algun de los libros disponibles.',
+'tradesr':'¿Que desea hacer ahora?\n¿Ver otro libro de Trades o regresar al menú',
+'Configuracion':'Eliga alguna de las siguientes opciones: \n\
+Temporizador: Ajustar el intervalo de tiempo entre mensajes automaticos.',
+'Timer':'Eliga un intervalo de tiempo o desactive los mensajes automaticos.',
+'Timerr':'Su configuracion ha sido cambiada. Puede volver a cambiar la configuracion o regresar al menu anterior.',
+'EmptyList':'Este libro esta vacio. Por favor intente con otro.'
 }
 
 #Lista de 'botones'.
 
 acuerdo = [
-{'text':'Acepto.','callback_data':'/Acepto.'},
-{'text':'No acepto.','callback_data':'/No acepto.'}
+{'text':'Acepto','callback_data':'Acepto'},
+{'text':'No acepto','callback_data':'No acepto'}
 ]
 
 acuerdopositivo = [
-{'text': 'AvailableBooks', 'callback_data': '/AvailableBooks'},
+{'text': 'Menu Bitso', 'callback_data': 'MenuBitso'},
+{'text': 'Configuracion', 'callback_data': 'Configuration'}
+]
+
+MenuBitso = [
+{'text': 'Libros disponibles', 'callback_data': '/AvailableBooks'},
 {'text': 'Ticker', 'callback_data': 'Ticker'},
-{'text': 'OrderBook', 'callback_data': 'OrderBook'},
-{'text': 'Trades', 'callback_data': 'Trades'}
+{'text': 'Libros de ordenes', 'callback_data': 'OrderBook'},
+{'text': 'Trades', 'callback_data': 'Trades'},
+{'text': 'Inicio', 'callback_data': 'Acepto'},
 ]
 
 acuerdonegativo = [
-{'text':'Cambié de opinión.','callback_data':'/Acepto.'}
+{'text':'Cambié de opinión.','callback_data':'/start'}
 ]
 
 AvailableBooks = [
-{'text':"Available Books",'callback_data':"/AvailableBooks"},
-{'text':"Regresar",'callback_data':"Pública."}
+{'text':"Libros disponibles",'callback_data':"/AvailableBooks"},
+{'text':"Regresar",'callback_data':"MenuBitso"}
 ]
 
 tickere = [
 {'text':'Ticker','callback_data':'Ticker'},
-{'text':'API Publica','callback_data':'Pública.'}
+{'text':'Regresar','callback_data':'MenuBitso'}
 ]
 
 orderbooke = [
-{'text':'Regresar a OrderBook','callback_data':'OrderBook'},
-{'text':'API Pública','callback_data':'Pública.'}
+{'text':'Ver otro libro','callback_data':'OrderBook'},
+{'text':'Regresar','callback_data':'MenuBitso'}
 ]
 
 tradese = [
-{'text':'Regresar a Trades','callback_data':'Trades'},
-{'text':'API Pública','callback_data':'Pública.'}
+{'text':'Ver otro libro','callback_data':'Trades'},
+{'text':'Regresar','callback_data':'Pública.'}
+]
+
+Configuration = [
+{'text':'Temporizador','callback_data':'Timer'},
+{'text':'Regresar','callback_data':'Acepto'}
+]
+
+InLineKeyBoardTimer = [
+{'text':'5 minutos','callback_data':'/Timer300'},
+{'text':'30 minutos','callback_data':'/Timer1800'},
+{'text':'1 hora','callback_data':'/Timer3600'},
+{'text':'5 horas','callback_data':'/Timer18000'},
+{'text':'Desactivar','callback_data':'/Timer0'},
+{'text':'Regresar','callback_data':'Configuration'}
 ]
 
 #FORMATOS DE RESPUESTA
--- main.py
+++ main.py
@@ -18,34 +18,34 @@ ltext = []
 URL = "https://api.telegram.org/bot{}/".format(telegram_token)
 
 #CONSULTAR ESTADO
-def get_url(url):
+def GetURL(url):
     response = requests.get(url)
     content = response.content.decode("utf8")
     return content
 
 #CAMBIAR DE JSON A PYTHON (PARSE)
-def get_json_from_url(url):
-    content = get_url(url)
+def GetJSONFromURL(url):
+    content = GetURL(url)
     js = json.loads(content)
     return js
 
 #SOLICITAR LISTA DE MENSAJES
-def get_updates(offset=None):
+def GetUpdates(offset=None):
     url = URL + "getUpdates?timeout=50"
     if offset:
         url += "&offset={}".format(offset)
-    js = get_json_from_url(url)
+    js = GetJSONFromURL(url)
     return js
 
 #DETERMINAR MENSAJES NO LEÍDOS
-def get_last_update_id(updates):
+def GetLasUpdateID(updates):
     update_ids = []
     for update in updates["result"]:
         update_ids.append(int(update["update_id"]))
     return max(update_ids)
 
 #CREAR TECLADO EN LINEA
-def build_inlinekeyboard(items):
+def BuildInlineKeyboard(items):
     nitems = []
     b = 0
     d = 2
@@ -71,48 +71,69 @@ def build_inlinekeyboard(items):
     reply_markup = {'inline_keyboard':keyboard}
     return json.dumps(reply_markup)
 
+def GetUserData(update):
+    if "callback_query" in update:
+        UID = update["callback_query"]["from"]["id"]
+        IsBot = update["callback_query"]["from"]["is_bot"]
+        FirstName = update["callback_query"]["from"]["first_name"]
+        LastName = update["callback_query"]["from"]["last_name"]
+        LanguageCode = update["callback_query"]["from"]["language_code"]
+    else:
+        UID = update["message"]["from"]["id"]
+        IsBot = update["message"]["from"]["is_bot"]
+        FirstName = update["message"]["from"]["first_name"]
+        LastName = update["message"]["from"]["last_name"]
+        LanguageCode = update["message"]["from"]["language_code"]
+    return UID, IsBot, FirstName, LastName, LanguageCode
+
 #MANEJAR A LOS USUARIOS
-def handle_users(updates):
+def HandleUsers(method = None, updates = None, TimerParam = None, UID = None):
     #AÑADIR NUEVOS USUARIOS
-    for update in updates["result"]:
-        if "callback_query" in update:
-            UId = update["callback_query"]["from"]["id"]
-            IsBot = update["callback_query"]["from"]["is_bot"]
-            FirstName = update["callback_query"]["from"]["first_name"]
-            LastName = update["callback_query"]["from"]["last_name"]
-            LanguageCode = update["callback_query"]["from"]["language_code"]
-            users = dbu.get_users()
-        else:
-            UId = update["message"]["from"]["id"]
-            IsBot = update["message"]["from"]["is_bot"]
-            FirstName = update["message"]["from"]["first_name"]
-            LastName = update["message"]["from"]["last_name"]
-            LanguageCode = update["message"]["from"]["language_code"]
-            users = dbu.get_users()
-        if UId not in users:
-            dbu.add_user(UId, IsBot, FirstName, LastName, LanguageCode)
+    if method == "general":
+        for update in updates["result"]:
+            if "callback_query" in update:
+                UID, IsBot, FirstName, LastName, LanguageCode = GetUserData(update)
+                users = dbu.get_users()
+            else:
+                UID, IsBot, FirstName, LastName, LanguageCode = GetUserData(update)
+                users = dbu.get_users()
+            if UID not in users:
+                Timer = 0
+                Time = 0
+                dbu.add_user(UID, IsBot, FirstName, LastName, LanguageCode, Timer, Time)
+    if method == "SetTimer":
+        for update in updates["result"]:
+            UID, IsBot, FirstName, LastName, LanguageCode = GetUserData(update)
+            Time = time.time()
+            dbu.SetTimer(TimerParam, UID, Time)
+
+# def CheckTimers():
+# print(gg)
+# pass
 
 #RESPONDER A TODOS LOS NO LEIDOS
-def handle_updates(updates):
+def HandleUpdates(updates):
     #EJECUTA EL MANEJO DE USUARIOS
-    handle_users(updates)
+    HandleUsers("general",updates)
     #DE LA RESPUESTA DEL SERVIDOR(JSON) DE TELEGRAM ASIGNA LOS VALORES A LAS VARIABLES.
+
     for update in updates["result"]:
         if "callback_query" in update:
             text = update["callback_query"]["data"]
             chat = update["callback_query"]["message"]["chat"]["id"]
-            UId = update["callback_query"]["from"]["id"]
+            UID = update["callback_query"]["from"]["id"]
             Date = update["callback_query"]["message"]["date"]
         else:
             text = update["message"]["text"]
             chat = update["message"]["chat"]["id"]
-            UId = update["message"]["from"]["id"]
+            UID = update["message"]["from"]["id"]
             Date = update["message"]["date"]
         #BUSCA QUÉ HACER CON EL TEXTO QUE LLEGA..
-        search_text(text, chat, UId)
+        Timer = dbu.CheckTime(UID)
+        SearchText(text, chat, UID, updates)
 
 #BUSCA LA INFORMACION SOLICITADA
-def retrieve_data(EndPoint, Book = None):
+def RetrieveData(EndPoint, Book = None):
     path = 'Data/Csvs/'
     sufix = '.csv'
     if EndPoint == 'AvailableBooks':
@@ -156,7 +177,7 @@ def CreateDictForKeyboar(list,EndPoint=None):
         CallBackData = '/' + EndPoint + i
         Format['callback_data'] = CallBackData
         Returner.append(Format)
-    Regresar = {'text': 'Regresar', 'callback_data': 'Pública.'}
+    Regresar = {'text': 'Regresar', 'callback_data': 'MenuBitso'}
     Returner.append(Regresar)
     return Returner
 
@@ -172,40 +193,32 @@ def OrderBookDictCreator(Book):
     return Returner
 
 #DETERMINAR QUE HACER A PARTIR DEL TEXTO RECIVIDO
-def search_text(text, chat, UId):
-    #print(text)
+def SearchText(text, chat, UID, updates):
+    print(text)
     ltext = text
     if text.startswith("/"):
         comando = text[1:]
         ##START##
         if comando == "start":
-            keyboard = build_inlinekeyboard(acuerdo)
-            send_message(mensajes["start"], chat, keyboard)
+            keyboard = BuildInlineKeyboard(acuerdo)
+            SendMessage(mensajes["start"], chat, keyboard)
         ##LIBROS DISPONIBLES##
         elif comando == "AvailableBooks":
-            libros = retrieve_data(comando)
+            libros = RetrieveData(comando)
             librob = []
             libroc = ""
             for libro in libros:
                 libroa = libro.replace('_',' - ')
                 librob.append(libroa)
                 librob.append("\n")
-            send_message(libroc.join(librob), chat)
-            keyboard = build_inlinekeyboard(AvailableBooks)
-            send_message(mensajes["availablebooksr"], chat, keyboard)
-        ##ACEPTO##
-        elif comando == "Acepto.":
-            keyboard = build_inlinekeyboard(acuerdopositivo)
-            send_message(mensajes["acepto"], chat, keyboard)
-        ##NO ACEPTO##
-        elif comando == "No acepto.":
-            keyboard = build_inlinekeyboard(acuerdonegativo)
-            send_message(mensajes["noacepto"], chat, keyboard)
+            SendMessage(libroc.join(librob), chat)
+            keyboard = BuildInlineKeyboard(AvailableBooks)
+            SendMessage(mensajes["availablebooksr"], chat, keyboard)
         ##TICKER##
         elif comando.startswith("Ticker"):
             book = comando[6:]
             EndPoint = comando[:6]
-            tick = retrieve_data(EndPoint, book)
+            tick = RetrieveData(EndPoint, book)
             ntick = 0
             atick = 0
             a = ""
@@ -217,38 +230,44 @@ def search_text(text, chat, UId):
                 ntick = ntick + 1
                 atick = atick + 1
             c = a.join(d)
-            send_message(c, chat)
-            keyboard = build_inlinekeyboard(tickere)
-            send_message(mensajes["tickere"], chat, keyboard)
+            SendMessage(c, chat)
+            keyboard = BuildInlineKeyboard(tickere)
+            SendMessage(mensajes["tickere"], chat, keyboard)
         ##ORDERBOOK##
         elif comando.startswith("OrderBook"):
             book = comando[9:]
             response = OrderBookDictCreator(book)
-            keyboard = build_inlinekeyboard(response)
-            send_message(mensajes["orderbookr"], chat, keyboard)
+            keyboard = BuildInlineKeyboard(response)
+            SendMessage(mensajes["orderbookr"], chat, keyboard)
         ##ORDERBOOK COMPRAS##
         elif comando.startswith("Compras"):
             try:
                 bbook = ltext
                 book = bbook[8:]
-                RawCompras = retrieve_data('OrderBook', book)
+                RawCompras = RetrieveData('OrderBook', book)
+                count = 0
                 Compras = []
                 for i in RawCompras:
                     if i[3] == 'bids':
+                        count += 1
                         Compras.append(i)
-                Texto = ""
-                for Compra in Compras:
-                    Lista = []
-                    Contador = 0
-                    for i in Compra:
-                        dos = orderbookrr[Contador].format(i)
-                        Lista.append(dos)
-                        Lista.append('\n')
-                        Contador += 1
-                    Mensaje = Texto.join(Lista)
-                    send_message(Mensaje, chat)
-                keyboard = build_inlinekeyboard(orderbooke)
-                send_message(mensajes["tickere"], chat, keyboard)
+                if count != 0:
+                    Texto = ""
+                    for Compra in Compras:
+                        Lista = []
+                        Contador = 0
+                        for i in Compra:
+                            dos = orderbookrr[Contador].format(i)
+                            Lista.append(dos)
+                            Lista.append('\n')
+                            Contador += 1
+                        Mensaje = Texto.join(Lista)
+                        SendMessage(Mensaje, chat)
+                    keyboard = BuildInlineKeyboard(orderbooke)
+                    SendMessage(mensajes["tickere"], chat, keyboard)
+                else:
+                    keyboard = BuildInlineKeyboard(orderbooke)
+                    SendMessage(mensajes["EmptyList"],chat,keyboard)
             except TypeError as e:
                 pass
         ##ORDERBOOK VENTAS##
@@ -256,31 +275,37 @@ def search_text(text, chat, UId):
             try:
                 bbook = ltext
                 book = bbook[7:]
-                RawVentas = retrieve_data('OrderBook', book)
+                RawVentas = RetrieveData('OrderBook', book)
+                count = 0
                 Ventas = []
                 for i in RawVentas:
                     if i[3] == 'asks':
+                        count += 1
                         Ventas.append(i)
-                Texto = ""
-                for Venta in Ventas:
-                    Lista = []
-                    Contador = 0
-                    for i in Venta:
-                        dos = orderbookrr[Contador].format(i)
-                        Lista.append(dos)
-                        Lista.append('\n')
-                        Contador += 1
-                    Mensaje = Texto.join(Lista)
-                    send_message(Mensaje, chat)
-                keyboard = build_inlinekeyboard(orderbooke)
-                send_message(mensajes["tickere"], chat, keyboard)
+                if count != 0:
+                    Texto = ""
+                    for Venta in Ventas:
+                        Lista = []
+                        Contador = 0
+                        for i in Venta:
+                            dos = orderbookrr[Contador].format(i)
+                            Lista.append(dos)
+                            Lista.append('\n')
+                            Contador += 1
+                        Mensaje = Texto.join(Lista)
+                        SendMessage(Mensaje, chat)
+                    keyboard = BuildInlineKeyboard(orderbooke)
+                    SendMessage(mensajes["tickere"], chat, keyboard)
+                else:
+                    keyboard = BuildInlineKeyboard(orderbooke)
+                    SendMessage(mensajes["EmptyList"],chat,keyboard)
             except TypeError as e:
-                send_message(str(e),chat)
+                SendMessage(str(e),chat)
         elif comando.startswith("Trades"):
             try:
                 bbook = ltext
                 book = bbook[7:]
-                Trades = retrieve_data('Trades',book)
+                Trades = RetrieveData('Trades',book)
                 Texto = ""
                 for Trade in Trades:
                     Lista = []
@@ -291,35 +316,51 @@ def search_text(text, chat, UId):
                         Lista.append('\n')
                         Contador += 1
                     Mensaje = Texto.join(Lista)
-                    send_message(Mensaje,chat)
-                keyboard = build_inlinekeyboard(tradese)
-                send_message(mensajes["tradesr"],chat,keyboard)
+                    SendMessage(Mensaje,chat)
+                keyboard = BuildInlineKeyboard(tradese)
+                SendMessage(mensajes["tradesr"],chat,keyboard)
             except TypeError as e:
-                send_message(str(e),chat)
+                SendMessage(str(e),chat)
+        elif comando.startswith("Timer"):
+            TimerParam = comando[5:]
+            HandleUsers("SetTimer", updates, TimerParam, UID)
+            keyboard = BuildInlineKeyboard(InLineKeyBoardTimer)
+            SendMessage(mensajes["Timerr"], chat, keyboard)
+    elif text == "Acepto":
+        keyboard = BuildInlineKeyboard(acuerdopositivo)
+        SendMessage(mensajes["acepto"], chat, keyboard)
+    elif text == "No acepto":
+        keyboard = BuildInlineKeyboard(acuerdonegativo)
+        SendMessage(mensajes["noacepto"], chat, keyboard)
     elif text == "Trades":
-        trades = retrieve_data('AvailableBooks')
+        trades = RetrieveData('AvailableBooks')
         TradesToSend = CreateDictForKeyboar(trades, text)
-        keyboard = build_inlinekeyboard(TradesToSend)
-        send_message(mensajes["trades"], chat, keyboard)
+        keyboard = BuildInlineKeyboard(TradesToSend)
+        SendMessage(mensajes["trades"], chat, keyboard)
     elif text == "Ticker":
-        libros = retrieve_data('AvailableBooks')
+        libros = RetrieveData('AvailableBooks')
         tickexxxx = CreateDictForKeyboar(libros, text)
-        keyboard = build_inlinekeyboard(tickexxxx)
-        send_message(mensajes["ticker"], chat, keyboard)
+        keyboard = BuildInlineKeyboard(tickexxxx)
+        SendMessage(mensajes["ticker"], chat, keyboard)
     elif text == "OrderBook":
-        orders = retrieve_data('AvailableBooks')
+        orders = RetrieveData('AvailableBooks')
         OrdersToSend = CreateDictForKeyboar(orders, text)
-        keyboard = build_inlinekeyboard(OrdersToSend)
-        send_message(mensajes["ticker"], chat, keyboard)
-    elif text == "Pública.":
-        keyboard = build_inlinekeyboard(acuerdopositivo)
-        send_message(mensajes["acepto"], chat, keyboard)
+        keyboard = BuildInlineKeyboard(OrdersToSend)
+        SendMessage(mensajes["ticker"], chat, keyboard)
+    elif text == "MenuBitso":
+        keyboard = BuildInlineKeyboard(MenuBitso)
+        SendMessage(mensajes["MenuBitso"], chat, keyboard)
+    elif text == "Configuration":
+        keyboard = BuildInlineKeyboard(Configuration)
+        SendMessage(mensajes["Configuracion"], chat, keyboard)
+    elif text == "Timer":
+        keyboard = BuildInlineKeyboard(InLineKeyBoardTimer)
+        SendMessage(mensajes["Timer"], chat, keyboard)
     else:
-        send_message("Entrada invalida. Intente de nuevo.", chat)
+        SendMessage("Entrada invalida. Intente de nuevo.", chat)
 
 #SOLICITAR ULTIMO MENSAJE Y ID DEL CHAT
-def get_last_chat_id_and_text(updates):
-    #print(updates)
+def GetLastChatIDAndText(updates):
     num_updates = len(updates["result"])
     lista = updates["result"]
     data = json.dumps(lista)
@@ -338,31 +379,31 @@ def get_last_chat_id_and_text(updates):
     return (text, chat_id)
 
 #ENVIAR MENSAJE
-def send_message(text, chat_id, reply_markup=None):
+def SendMessage(text, chat_id, reply_markup=None):
     text = text.encode(encoding='utf-8')
     text = urllib.parse.quote_plus(text)
     url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
     if reply_markup:
         url += "&reply_markup={}".format(reply_markup)
-    get_url(url)
+    GetURL(url)
 
-text, chat = get_last_chat_id_and_text(get_updates())
-send_message(text, chat)
+text, chat = GetLastChatIDAndText(GetUpdates())
+SendMessage(text, chat)
 
 ##EJECUTAR
-def main():
+def Main():
     try:
         dbu.setup()
         last_update_id = None
         while True:
-            updates = get_updates(last_update_id)
+            updates = GetUpdates(last_update_id)
             if len(updates["result"]) > 0:
-                last_update_id = get_last_update_id(updates) + 1
-                handle_updates(updates)
+                last_update_id = GetLasUpdateID(updates) + 1
+                HandleUpdates(updates)
             time.sleep(0.5)
     except Exception as e:
         raise e
 
 #CONDICION PARA EJECUTAR
 if __name__ == '__main__':
-    main()
+    Main()
